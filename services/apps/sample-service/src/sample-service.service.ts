03dc97d2a25557dff0b8d5945b5884a28d964d67
trungng29 <qtnk2912@gmail.com>
2026-08-24 20:12:25 +0700

    [Services][Authentication] Setup authentication service folders structure
    Signed-off-by: trungng29 <qtnk2912@gmail.com>

--- services/apps/sample-service/src/sample-service.service.ts
+++ services/apps/sample-service/src/sample-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class SampleServiceService {
   getHello(): string {
     return 'Hello World!';
   }
--- services/apps/sample-service/test/app.e2e-spec.ts
+++ services/apps/sample-service/test/app.e2e-spec.ts
@@ -1,15 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import * as request from 'supertest';
+import { SampleServiceModule } from './../src/sample-service.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('SampleServiceController (e2e)', () => {
+  let app: INestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [SampleServiceModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -21,9 +20,5 @@ describe('AppController (e2e)', () => {
       .get('/')
       .expect(200)
       .expect('Hello World!');
-  });
-
-  afterEach(async () => {
-    await app.close();
   });
 });
